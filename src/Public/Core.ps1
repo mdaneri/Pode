4e4cdf43e104664be31cb39311f9551918ba446a
mdaneri <max.daneri@broadcom.com>
2024-11-28 07:25:56 -0800

    fix pipename generator on mac/linux+ log errors

--- src/Public/Core.ps1
+++ src/Public/Core.ps1
@@ -238,6 +238,7 @@ function Start-PodeServer {
                     Quiet              = $PodeService.Quiet
                     PipeName           = $PodeService.PipeName
                 }
+                write-podehost $PodeService -Explode -Force
             }
         }
 
