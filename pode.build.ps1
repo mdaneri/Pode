acc27190e8eb978d280586627850ed163f2c0b7f
mdaneri <max.daneri@broadcom.com>
2024-12-24 08:21:01 -0800

    Update pode.build.ps1

--- pode.build.ps1
+++ pode.build.ps1
@@ -106,9 +106,6 @@ param(
     [switch]
     $DisableLifecycleServiceOperations,
 
-    [switch]
-    $DisableLifecycleServiceOperations,
-
     [string[]]
     [ValidateSet('netstandard2.0', 'net8.0', 'net9.0', 'net10.0')]
     $TargetFrameworks = @('netstandard2.0', 'net8.0', 'net9.0'),
