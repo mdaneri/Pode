1ee0d313ec0fac5214772c1e81b13aa21415babf
mdaneri <max.daneri@broadcom.com>
2025-03-10 19:26:48 -0700

    PR 1473

--- src/Pode.psm1
+++ src/Pode.psm1
@@ -102,6 +102,7 @@ try {
 
     # Import the module manifest to access its properties
     $PodeManifest = Import-PowerShellDataFile -Path $moduleManifestPath -ErrorAction Stop
+    $PodeManifest = Import-PowerShellDataFile -Path $moduleManifestPath -ErrorAction Stop
 
     if (! (Test-PodeAssembly)) {
         # fetch the .net version and the libs path
